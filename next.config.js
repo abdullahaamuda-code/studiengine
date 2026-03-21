991cc168528fe4dcbd4d010819781b3f93cb2326
abdullahaamuda-code <abdullahaamuda@gmail.com>
2026-03-21 23:46:59 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,20 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config, { isServer }) => {
-    // Prevent canvas errors
     config.resolve.alias.canvas = false;
-
-    if (!isServer) {
-      config.module.rules.push({
-        test: /pdf\.worker\.(min\.)?mjs$/,
-        type: "asset/resource",
-        generator: { filename: "static/chunks/[name].[hash][ext]" },
-      });
-    }
-
     return config;
   },
-  // Tell Next.js not to bundle pdfjs on the server
   experimental: {
     serverComponentsExternalPackages: ["pdfjs-dist"],
   },
