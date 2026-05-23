6a27f37da6b0b7d107e1aecc3fdb0f6870371e8d
abdullahaamuda-code <abdullahaamuda@gmail.com>
2026-05-23 00:54:59 -0700

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "studiengine-v1";
+const CACHE = "studiengine-v2026-05-23-05";
 const STATIC = ["/", "/manifest.json", "/icon-192.png", "/icon-512.png"];
 
 self.addEventListener("install", e => {
