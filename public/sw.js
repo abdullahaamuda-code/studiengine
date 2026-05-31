d169724f160ef3c09034f195f0c4cee0989a7a50
abdullahaamuda-code <abdullahaamuda@gmail.com>
2026-05-31 04:59:34 -0700

    Update cache version in service worker

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "studiengine-v2026-05-23-05";
+const CACHE = "studiengine-v3";
 const STATIC = ["/", "/manifest.json", "/icon-192.png", "/icon-512.png"];
 
 self.addEventListener("install", e => {
